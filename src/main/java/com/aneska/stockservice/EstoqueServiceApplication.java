541a893cebc95e55af50fedcca7a04601084f8f4
aneskak <aneska.karina@hotmail.com>
2024-07-29 21:01:41 -0300

    stock kafka consumer

---
+++ src/main/java/com/aneska/stockservice/EstoqueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aneska.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueServiceApplication.class, args);
+	}
+
+}
